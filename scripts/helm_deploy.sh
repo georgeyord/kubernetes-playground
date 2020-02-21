3597e25e529272e89d43c8d7ed82bd060e6d68a7
George Yord <george.yord@gmail.com>
2020-02-21 10:53:11 +0200

    :zap: Bypass helm update on demand

--- scripts/helm_deploy.sh
+++ scripts/helm_deploy.sh
@@ -4,6 +4,9 @@ set -e
 
 SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
 
+export HELM_DEP_IGNORE="${HELM_DEP_IGNORE:-0}"
+if [[ "$1" == "-f" ]]; then HELM_DEP_IGNORE=1; shift; fi
+
 "${SCRIPTS_DIR}/helm_dep_update.sh"
 
 helm lint
