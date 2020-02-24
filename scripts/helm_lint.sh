a10c0d9655fadb05ea0b12174c5ea22ee3824512
George Yord <george.yord@gmail.com>
2020-02-24 17:25:22 +0200

    :recycle: Refactor hem lint script

--- scripts/helm_lint.sh
+++ scripts/helm_lint.sh
@@ -9,14 +9,14 @@ if [[ "$1" == "-f" ]]; then HELM_NO_PREPARE=1; shift; fi
 
 "${SCRIPTS_DIR}/helm_prepare.sh"
 
-echo >&2 -e "\n########## Release deploy - experiment ########## \n"
+lint() {
+  local RELEASE="$1"
+  echo >&2 -e "\n########## Release lint - ${RELEASE} ########## \n"
 
-helm lint . \
-  -f values.experiment.yaml \
-  -f values.custom.yaml
+  helm lint . \
+    -f "values.${RELEASE}.yaml" \
+    -f values.custom.yaml
+}
 
-echo >&2 -e "\n########## Release deploy - cert-manager ########## \n"
-
-helm lint . \
-  -f values.cert-manager.yaml \
-  -f values.custom.yaml
+lint 'experiment'
+lint 'cert-manager'
